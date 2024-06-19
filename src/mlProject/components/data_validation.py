28f73449f7d89e38471ef91e5789955a8845009f
Aditya Sanyal <adityasanyal1996@gmail.com>
2024-06-19 14:29:24 +0530

    added functionality to data validation and connected it with the data transformation pipeline

--- src/mlProject/components/data_validation.py
+++ src/mlProject/components/data_validation.py
@@ -5,50 +5,57 @@ from mlProject.entity.config_entity import DataValidationConfig
 
 
 
-
 class DataValidation:
     def __init__(self, config : DataValidationConfig):
         self.config = config
 
     def validate_all_columns(self)->bool:
         try:
-            validation_status = None
+            validation_status = True #initially stays true
 
             #we want to erase the contents from the previous run
             with open(self.config.STATUS_FILE, 'w') as f:
                 pass
 
 
-            all_schema_types = []
+            #all_schema_types = []
             data = pd.read_csv(self.config.unzip_data_dir)
             all_cols = list(data.columns)
-            all_schema = self.config.all_schema.keys()
-           # all_schema_types = self.config.all_schema.values()
+            all_schema_keys = self.config.all_schema.keys()
             
-            for element in self.config.all_schema.values():
-                all_schema_types.append(str(element))
             #for validating the data types
-            
-            for col in all_cols:
-                if str(data.dtypes[(col)]) not in all_schema_types: #not entering the if
-                    validation_status = False
-                    break
-                    
+            size = len(all_schema_keys) # for iterating through all the columns in the dataset
+            count = 0 # keeping a track of iteratiion through the dataset
+            for col in all_cols:            
+                count += 1
+                for key in list(all_schema_keys):
+                    if (col == key):
+                        if (str(data.dtypes[col]) != (self.config.all_schema)[key]):
+                            validation_status  = False
+                            logger.info(f"{col} type in dataset doesn't match with {key} in schema")
+                            break
+
+                #if type doesn't match, the control will come outside(here!)
+
+                if count == size: #we need to check for all the cols and keys at once
+                    pass #since it is the end of iteration, validation_status will remain True if no 
+                         #mismatch occurs, otherwise the validation_status will become false
                 else:
-                    validation_status = True
+                    continue
+
 
             with open(self.config.STATUS_FILE, 'a') as f:
                 f.write(f"Type Validation status: {validation_status}")
 
                 f.write("\n")
             
+            validation_status = True
             #for validating the columns
             for col in all_cols:
-                if col not in all_schema:
+                if col not in all_schema_keys:
                     validation_status = False
-                    break
-                else:
-                    validation_status = True
+                    logger.info(f"{col} column doesn't match with schema")
+
             
             with open(self.config.STATUS_FILE, 'a') as f:
                 f.write(f"Column Validation status: {validation_status}")
--- src/mlProject/pipeline/stage_03_data_transformation.py
+++ src/mlProject/pipeline/stage_03_data_transformation.py
@@ -14,24 +14,16 @@ class DataTransformationTrainingPipeline:
 
 
     def main(self):
-        try:
-            with open(Path("artifacts/data_validation/status.txt"), "r") as f:
-                status = f.read().split(" ")[-1]
-
-            if status == "True":
-                config = ConfigurationManager()
-                data_transformation_config = config.get_data_transformation_config()
-                data_transformation = DataTransformation(config=data_transformation_config)
-                data_transformation.train_test_spliting()
-
-            else:
+        with open(Path("artifacts/data_validation/status.txt"), "r") as f:
+            status_type = f.readline().split(" ")[-1]
+            status_column = f.readline().split(" ")[-1]
+        if (status_type=="True\n" and status_column == "True"):
+            config = ConfigurationManager()
+            data_transformation_config = config.get_data_transformation_config()
+            data_transformation = DataTransformation(config=data_transformation_config)
+            data_transformation.train_test_spliting()
+        else:
                 raise Exception("You data schema is not valid")
-
-        except Exception as e:
-            print(e)
-
-
-
 
 
 if __name__ == '__main__':
@@ -41,6 +33,7 @@ if __name__ == '__main__':
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
+        str(e)
         logger.exception(e)
         raise e
 
