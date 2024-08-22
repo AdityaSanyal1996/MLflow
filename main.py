425f16b5a5fe50feaac036a7bffe6e95c656eff6
Aditya <adityasanyal1996@gmail.com>
2024-08-22 21:30:27 +0530

    added the evaluation pipeline using mlflow

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipe
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from mlProject.pipeline.stage_04_model_training import ModelTrainingPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -47,4 +48,16 @@ except Exception as e:
     str(e)
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Model Evaluation stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        str(e)
+        logger.exception(e)
+        raise e
 
